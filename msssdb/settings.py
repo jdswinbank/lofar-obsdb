a4e4476cbb66ad234b2445562ef40310c5b6c8ce
John Swinbank <swinbank@transientskp.org>
2012-06-25 17:42:15 +0200

    First bash at a db layout

--- msssdb/settings.py
+++ msssdb/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.staticfiles',
+    'observationdb'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- observationdb/models.py
+++ observationdb/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Survey(models.Model):
+    name = models.CharField(primary_key=True, max_length=100)
+
+class Field(models.Model):
+    name = models.CharField(primary_key=True, max_length=100)
+    ra = models.FloatField()
+    dec = models.FloatField()
+    survey = models.ForeignKey(Survey)
+
+class Station(models.Model):
+    name = models.CharField(primary_key=True, max_length=10)
+
+class Observation(models.Model):
+    obsid = models.CharField(primary_key=True, max_length=10)
+    field = models.ForeignKey(Field)
+    stations = models.ManyToManyField(Station)
+    start_time = models.DateTimeField()
+    duration = models.IntegerField() # Always an integral number of seconds?
+
+class Beam(models.Model):
+    observation = models.ForeignKey(Observation)
+    beam = models.IntegerField()
+    calibrators = models.ManyToManyField('self', blank=True)
