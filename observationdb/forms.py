2944e9120d8051c6affd9fce9061c5972532a1f1
John Swinbank <swinbank@transientskp.org>
2012-07-11 16:45:05 +0200

    Prettier filtering

--- observationdb/forms.py
+++ observationdb/forms.py
@@ -3,11 +3,10 @@ from django import forms
 from observationdb.models import Survey
 
 class FieldFilterForm(forms.Form):
-    ra = forms.FloatField(required=False)
-    dec = forms.FloatField(required=False)
-    radius = forms.FloatField(required=False)
-    survey = forms.ChoiceField(
-        choices=[(s.id, s.name) for s in Survey.objects.all()],
-        required=False
-    )
-
+    ra = forms.FloatField(required=False, label="Right Ascension")
+    dec = forms.FloatField(required=False, label="Declination")
+    radius = forms.FloatField(required=False, label="Search Radius")
+#    survey = forms.ChoiceField(
+#        choices=[(s.id, s.name) for s in Survey.objects.all()],
+#        required=False
+#    )
