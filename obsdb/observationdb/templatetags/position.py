84852c02351231f4d4c1bcfc155e397452ed8dce
John Swinbank <swinbank@transientskp.org>
2012-07-21 19:33:43 +0200

    Use our own routines for formating RA & dec
    This removes all dependency on pyrap from the website. It is still used for
    parsing & loading data files, however.

--- obsdb/observationdb/templatetags/position.py
+++ obsdb/observationdb/templatetags/position.py
@@ -1,13 +1,21 @@
 from django import template
-from pyrap.quanta import quantity
+from django.utils.safestring import mark_safe
+
 import math
+from ..utils import radians_to_hms
+from ..utils import radians_to_dms
 
 register = template.Library()
 
 @register.filter
 def format_angle(value, format_type):
-    q = quantity("%frad" % value)
-    return q.formatted(str(format_type))
+    if format_type == "time":
+        h, m, s = radians_to_hms(float(value))
+        result = "%d<sup>h</sup> %d<sup>m</sup> %.1f<sup>s</sup>" % (h, m, s)
+    if format_type == "dms":
+        sign, d, m, s = radians_to_dms(float(value))
+        result = "%s%d&deg; %d&prime; %.1f&Prime;" % (sign, d, m, s)
+    return mark_safe(result)
 
 @register.filter
 def to_degrees(value):
---
+++ obsdb/observationdb/utils.py
@@ -0,0 +1,38 @@
+import math
+
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 60**2
+SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
+RADIANS_IN_CIRCLE = 2 * math.pi
+
+ASEC_IN_AMIN = 60
+ASEC_IN_DEGREE = 60**2
+
+def radians_to_hms(rad):
+    rad = rad % (RADIANS_IN_CIRCLE)
+    seconds = SECONDS_IN_DAY * rad / RADIANS_IN_CIRCLE
+    hours, seconds = divmod(seconds, SECONDS_IN_HOUR)
+    minutes, seconds = divmod(seconds, SECONDS_IN_MINUTE)
+    return hours, minutes, seconds
+
+def radians_to_dms(rad):
+    sign = "+" if rad >= 0 else "-"
+    rad = abs(rad)
+    seconds = math.degrees(rad) * ASEC_IN_DEGREE
+    degrees, seconds = divmod(seconds, ASEC_IN_DEGREE)
+    minutes, seconds = divmod(seconds, ASEC_IN_AMIN)
+    return sign, degrees, minutes, seconds
+
+def hms_to_radians(h, m, s):
+    seconds = SECONDS_IN_HOUR * h +   \
+              SECONDS_IN_MINUTE * m + \
+              s
+    return RADIANS_IN_CIRCLE * seconds / SECONDS_IN_DAY
+
+def dms_to_radians(d, m ,s):
+    sign = 1 if d >= 0 else -1
+    d = abs(d)
+    seconds = ASEC_IN_DEGREE * d + \
+              ASEC_IN_AMIN * m +   \
+              s
+    return sign * math.degrees(seconds / ASEC_IN_DEGREE)
