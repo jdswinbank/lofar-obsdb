b9a2f85b9088280254da67383a07b6c601ede760
John Swinbank <swinbank@transientskp.org>
2012-07-21 14:17:09 +0200

    Reorganize setup scripts

---
+++ prime_db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Script to prime the database based on existing metadata and parsets.
+# Once the project is complete using fixtures will be more convenient, but
+# this enables us to more easily redesign & reinitialize the data while
+# developing.
+
+WORKING_DIR=`dirname ${0}`
+SCRIPT_DIR=${WORKING_DIR}/scripts
+METADATA_DIR=${WORKING_DIR}/metadata
+
+export PYTHONPATH=${WORKING_DIR}${PYTHONPATH:+:${PYTHONPATH}}
+export DJANGO_SETTINGS_MODULE="msssdb.settings"
+
+python ${SCRIPT_DIR}/create_stations.py ${METADATA_DIR}/StationInfo.dat
+python ${SCRIPT_DIR}/create_survey.py "MSSS LBA" ${METADATA_DIR}/calibrators.lba.txt ${METADATA_DIR}/grid.lba.txt
+python ${SCRIPT_DIR}/create_survey.py "MSSS HBA" ${METADATA_DIR}/calibrators.hba.txt ${METADATA_DIR}/grid.hba.txt
+python ${SCRIPT_DIR}/load_data.py
