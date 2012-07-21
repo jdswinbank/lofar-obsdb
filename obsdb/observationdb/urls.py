01fa00dc8c943de0840ffa986d548c738c8a5b48
John Swinbank <swinbank@transientskp.org>
2012-07-21 20:52:09 +0200

    GET rather than POST to page through observations

--- obsdb/observationdb/urls.py
+++ obsdb/observationdb/urls.py
@@ -20,7 +20,6 @@ urlpatterns = patterns('obsdb.observationdb.views',
     url(r'^field/$', 'field_list'),
 
     # Observations
-    url(r'^observation/$', redirect_to, {'url': '/observation/page1'}),
     url(r'^observation/(?P<pk>L\d+)/$',
         DetailView.as_view(
             model=Observation,
@@ -28,12 +27,12 @@ urlpatterns = patterns('obsdb.observationdb.views',
         ),
         name="observation_detail"
     ),
-    url(r'^observation/page(?P<page>\d+)/$',
+    url(r'^observation/$',
         ListView.as_view(
             queryset=Observation.objects.all(),
             context_object_name='obs_list',
             template_name='observation_list.html',
-            paginate_by=200,
+            paginate_by=100,
         ),
         name="obs_list"
     ),
