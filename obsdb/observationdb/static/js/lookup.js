ac60bf999730951bc01df4975fb68478fcb88687
John Swinbank <swinbank@transientskp.org>
2012-11-28 23:48:41 +0100

    Escape object names in URIs

--- obsdb/observationdb/static/js/lookup.js
+++ obsdb/observationdb/static/js/lookup.js
@@ -41,7 +41,7 @@ function lookUP(form) {
 		var headID = document.getElementsByTagName("head")[0];
 		var newScript = document.createElement('script');
 		newScript.type = 'text/javascript';
-		newScript.src = 'http://www.strudel.org.uk/lookUP/json/?name='+object+'&callback=getLookUPResults';
+		newScript.src = 'http://www.strudel.org.uk/lookUP/json/?name='+encodeURIComponent(object)+'&callback=getLookUPResults';
 		headID.appendChild(newScript);
 	}
 }
