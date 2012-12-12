147d3a884dfb6c0f93af016976caa5e6829009f0
John Swinbank <swinbank@transientskp.org>
2012-12-12 12:17:28 +0100

    Convert to jpg for size reasons

--- obsdb/settings.py
+++ obsdb/settings.py
@@ -151,5 +151,5 @@ PAGE_SIZE = 100
 SPLASH_IMAGES = [
     (STATIC_URL + "img/LOFAR_Superterp.jpg", ""),
     (STATIC_URL + "img/LBA.jpg", "LOFAR LBA,  &copy; Hans Hordijk"),
-    (STATIC_URL + "img/heald-msss.png", "")
+    (STATIC_URL + "img/heald-msss.jpg", "")
 ]
