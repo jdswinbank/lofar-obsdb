94b61d497fe437a323c120d31ac30275a8835e5c
John Swinbank <swinbank@transientskp.org>
2012-12-12 11:59:15 +0100

    Add LBA picture

--- obsdb/settings.py
+++ obsdb/settings.py
@@ -151,4 +151,5 @@ PAGE_SIZE = 100
 SPLASH_IMAGES = [
     (STATIC_URL + "img/CygnusA.jpg", "J. McKean & M. Wise"),
     (STATIC_URL + "img/LOFAR_Superterp.jpg", "")
+    (STATIC_URL + "img/LBA.jpg", "LOFAR LBA, © Hans Hordijk")
 ]
